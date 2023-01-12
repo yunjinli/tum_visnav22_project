1ce89a4e546f663e5ea7dcf901019ccf446ea333
yunjinli <yunjin.li0817@gmail.com>
2023-01-12 18:58:51 +0100

    restore wrong commit

--- test/src/test_imu_preintegration.cpp
+++ test/src/test_imu_preintegration.cpp
@@ -41,7 +41,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 namespace visnav::constants {
 static const Eigen::Vector3d G(0, 0, -9.81);
 }  // namespace visnav::constants
-}  // namespace visnav::constants
 
 static const double ACCEL_STD_DEV = 0.23;
 static const double GYRO_STD_DEV = 0.0027;
